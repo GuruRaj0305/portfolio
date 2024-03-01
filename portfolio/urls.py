a51b8478a6f8b9b8feb7ef7eacea9b4328fb1318
GuruRaj0305 <gururajhr0305l@gmail.com>
2024-03-01 07:38:49 +0530

    this complete ready to deploy with postgres db..

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,13 +19,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("mywebpage.urls")),
-    url(r'^media/(?<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)\
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
